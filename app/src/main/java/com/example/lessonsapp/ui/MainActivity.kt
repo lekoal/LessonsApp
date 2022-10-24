054dd0dce9a9c951bb7b86b44a7128628b6c28c1
lekoal <coreglus@gmail.com>
2022-10-24 11:35:40 +0300

    ViewPager2 & TabsLayout are added

--- app/src/main/java/com/example/lessonsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/lessonsapp/ui/MainActivity.kt
@@ -2,14 +2,24 @@ package com.example.lessonsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.lessonsapp.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val tabTitles = arrayOf("Home", "Classes")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        TabLayoutMediator(
+            binding.tabLayout, binding.viewPager
+        ) { tab, position -> tab.text = tabTitles[position] }.attach()
     }
 }
--- app/src/main/java/com/example/lessonsapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/lessonsapp/ui/main/MainFragment.kt
@@ -1,28 +1,11 @@
 package com.example.lessonsapp.ui.main
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.lessonsapp.R
 import com.example.lessonsapp.databinding.FragmentMainBinding
+import com.example.lessonsapp.utils.ViewBindingFragment
 
-class MainFragment : Fragment() {
-
-    private var _binding: FragmentMainBinding? = null
-    private val binding = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+class MainFragment : ViewBindingFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
 
     companion object {
-        fun newInstance() = MainFragment()
 
     }
 }
