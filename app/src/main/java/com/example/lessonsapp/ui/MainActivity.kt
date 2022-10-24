d93b8472b7ac9c97807fd3f8ad94c94c0f763792
lekoal <coreglus@gmail.com>
2022-10-24 16:32:35 +0300

    Need to fix initial tab showing

--- app/src/main/java/com/example/lessonsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/lessonsapp/ui/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.lessonsapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.example.lessonsapp.R
 import com.example.lessonsapp.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    private val tabTitles = arrayOf("Home", "Classes")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +20,45 @@ class MainActivity : AppCompatActivity() {
         binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
         TabLayoutMediator(
             binding.tabLayout, binding.viewPager
-        ) { tab, position -> tab.text = tabTitles[position] }.attach()
+        ) { _, _ -> }.attach()
+        setCustomTabs()
+        activeTabListener()
+    }
+
+    companion object {
+        private const val HOME = 0
+        private const val CLASSES = 1
+    }
+
+    private fun setCustomTabs() {
+        binding.tabLayout.getTabAt(HOME)?.setCustomView(R.layout.view_pager_tab_home_selected)
+        binding.tabLayout.getTabAt(CLASSES)?.setCustomView(R.layout.view_pager_tab_classes)
+    }
+
+    private fun activeTabListener() {
+        binding.viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                setHighLightedTab(position)
+            }
+        })
+    }
+
+    private fun setHighLightedTab(position: Int) {
+        when (position) {
+            HOME -> {
+                binding.tabLayout.getTabAt(HOME)?.customView = null
+                binding.tabLayout.getTabAt(HOME)
+                    ?.setCustomView(R.layout.view_pager_tab_home_selected)
+                binding.tabLayout.getTabAt(CLASSES)?.customView = null
+                binding.tabLayout.getTabAt(CLASSES)?.setCustomView(R.layout.view_pager_tab_classes)
+            }
+            CLASSES -> {
+                binding.tabLayout.getTabAt(HOME)?.customView = null
+                binding.tabLayout.getTabAt(HOME)?.setCustomView(R.layout.view_pager_tab_home)
+                binding.tabLayout.getTabAt(CLASSES)?.customView = null
+                binding.tabLayout.getTabAt(CLASSES)
+                    ?.setCustomView(R.layout.view_pager_tab_classes_selected)
+            }
+        }
     }
 }
